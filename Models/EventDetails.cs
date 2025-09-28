6a51ddebfb873968d5738725cd5b44a20e50716d
rajanishaa006 <rajanishaa006@gmail.com>
2025-09-28 22:15:07 +0530

    Event Creation API

--- Models/EventDetails.cs
+++ Models/EventDetails.cs
@@ -7,15 +7,15 @@
         public required string Description { get; set; }
         public List<string>? Tags { get; set; }
         public string? MeetingLink { get; set; }
-        public int SendLinkNotificationAt { get; set; } //1-> send meet link at a physical push of a button , 2->send meeting link when Is_accepting_participants is turned off, 3-> send when user Applies/isApproved for the event
-        public EventFeedback? EventFeedback { get; set; }
+        public required int SendLinkNotificationAt { get; set; } //1-> send meet link at a physical push of a button , 2->send meeting link when Is_accepting_participants is turned off, 3-> send when user Applies/isApproved for the event
+        public required EventFeedback EventFeedback { get; set; }
         public required OrganizerDetails OrganizerDetails { get; set; }
         public required long StartTimestamp { get; set; }
         public required long EndTimestamp { get; set; }
         public required long CreatedTimestamp { get; set; }
         public required long UpdatedTimestamp { get; set; }
         public required bool IsPrivate { get; set; }
-        public required List<ParticipantDetails> Participants { get; set; }
+        public List<ParticipantDetails>? Participants { get; set; }
         public required bool Is_accepting_participants { get; set; }
         public int TotalRegisteredParticipants { get; set; }
         public required int Active { get; set; } //1 -> upcoming, 0 -> past, -1 -> cancelled
--- Models/EventFeedback.cs
+++ Models/EventFeedback.cs
@@ -5,10 +5,20 @@
         public required string EventId { get; set; }
         public required bool EnableRating { get; set; }
         public required bool EnableComments { get; set; }
-        public int TotalRaings { get; set; } // Total number of ratings received
-        public float? Rating { get; set; } // Average ratings 12
+        public int TotalRatings { get; set; } // Total number of ratings received
+        public List<Ratings>? ParticipantRatings {  get; set; }
+        public required float AverageRating { get; set; } // Average ratings
         public List<Comments>? Comments { get; set; }
     }
+
+    public class Ratings
+    {
+        public required string UserId { get; set; }
+        public required int ParticipantRating { get; set; }
+        public required long CreatedTimestamp { get; set; }
+        public required long UpdatedTimestamp { get; set; }
+    }
+
 
     public class Comments
     {
@@ -18,5 +28,6 @@
         public required string ForegroundColour { get; set; }
         public required string Comment { get; set; }
         public required long CreatedTimestamp { get; set; }
+        public required long UpdatedTimestamp { get; set; }
     }
 }
